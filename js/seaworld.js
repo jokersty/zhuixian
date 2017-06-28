8f0227f7d823082b189c0d5c04378746943a3ae0
Your Name <you@example.com>
2017-06-28 18:17:49 +0800

    s

---
+++ js/seaworld.js
@@ -0,0 +1,8 @@
+// $('#search').click(function () {
+//     window.location.href = "#search";
+//     $('.classify').hide();
+//     $('.list').hide()
+// })
+$('#searchway').click(function () {
+    $.router.load("#router2");
+})
