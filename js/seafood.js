8593b51af8f8b5fc86d6b4b36ead3dfe8ad4e85b
Your Name <you@example.com>
2017-06-30 17:21:04 +0800

    a

--- js/seafood.js
+++ js/seafood.js
@@ -1,12 +1,77 @@
-$('#toSeafood').click(function () {
-    window.location.href = "feast.html";
+var seafood = (function () {
+
+    function seafood(content) {
+        $.showIndicator();
+        $.ajax({
+            url: "",
+            data: content,
+            success: function (result) {
+                result = {data:[
+                    {imgsrc: "img/yellow-croaker.png"},
+                    {imgsrc: "img/yellow-croaker.png"},
+                    {imgsrc: "img/yellow-croaker.png"},
+                    {imgsrc: "img/yellow-croaker.png"}
+                    ]};
+                if(result.data) {
+                    $("#food").html(template('foodList', result));
+                }
+                else if(result.data == "" || !result.data) {
+                    $.alert("没有相关菜谱");
+                }
+            },
+            error : function () {
+                $.alert('服务器错误');
+            },
+            complete: function() {
+                $.hideIndicator();
+            }
+        });
+    }
+
+    function init() {
+        var content = {
+            type: "" ,
+            dish: ""
+        };
+        content.type = 1;
+        seafood(content);
+
+        $('.classify-content').find('li').on("click", function () {
+            var type = $(this).val();
+            content.type = type;
+            seafood(content);
+        })
+        $('#goSearch').click(function () {
+            var dish = $('#search').val();
+            if(dish == "") {
+                $.alert("请输入您要查询的菜谱")
+            }else {
+                $.router.back();
+                content.type = "";
+                content.dish = dish;
+                seafood(content);
+            }
+        });
+        $('#toSeafood').click(function () {
+            window.location.href = "feast.html";
+        });
+        $('#searchway').click(function () {
+            $.router.load("#router2");
+        })
+        $('#searchtextBack').click(function () {
+            $.router.back();
+        })
+        $('.searchway-input').on('click',function () {
+            $('#search').focus();
+        })
+    }
+
+    return {
+        init: init
+    }
+})();
+
+$(function () {
+    $.showIndicator();
+    seafood.init();
 });
-$('#searchway').click(function () {
-    $.router.load("#router2");
-})
-$('#searchtextBack').click(function () {
-    $.router.back();
-})
-$('.searchway-input').on('click',function () {
-    $('#search').focus();
-})
--- js/seaworld.js
+++ js/seaworld.js
@@ -1,14 +1,74 @@
-// $('#search').click(function () {
-//     window.location.href = "#search";
-//     $('.classify').hide();
-//     $('.list').hide()
-// })
-$('#searchway').click(function () {
-    $.router.load("#router2");
-})
-$('#searchtextBack').click(function () {
-    $.router.back();
-})
-$('.searchway-input').on('click',function () {
-    $('#search').focus();
-})
+var seaworld = (function () {
+
+    function seaworld(content) {
+        $.showIndicator();
+        $.ajax({
+            url: "",
+            data: content,
+            success: function (result) {
+                result = {data:[{imgsrc: "img/fish-dinner.png", fishName: "黄鱼", fishText: "黄鱼，有大小黄鱼之分，又名黄花鱼。"},
+                    {imgsrc: "img/fish-dinner.png", fishName: "黄鱼", fishText: "黄鱼，有大小黄鱼之分，又名黄花鱼。"},
+                    {imgsrc: "img/fish-dinner.png", fishName: "黄鱼", fishText: "黄鱼，有大小黄鱼之分，又名黄花鱼。"},
+                    {imgsrc: "img/fish-dinner.png", fishName: "黄鱼", fishText: "黄鱼，有大小黄鱼之分，又名黄花鱼。"},
+                    {imgsrc: "img/fish-dinner.png", fishName: "黄鱼", fishText: "黄鱼，有大小黄鱼之分，又名黄花鱼。"},
+                    {imgsrc: "img/fish-dinner.png", fishName: "黄鱼", fishText: "黄鱼，有大小黄鱼之分，又名黄花鱼。"}]};
+                if(result.data) {
+                    $("#List").html(template('fishList', result));
+                }
+                else if(result.data == "" || !result.data) {
+                    $.alert("没有相关海鲜");
+                }
+            },
+            error : function () {
+                $.alert('服务器错误');
+            },
+            complete: function() {
+                $.hideIndicator();
+            }
+        });
+    }
+
+    function init() {
+        var content = {
+            type: "" ,
+            dish: ""
+        };
+        content.type = 1;
+        seaworld(content);
+
+        $('.classify-content').find('li').on("click", function () {
+            var type = $(this).val();
+            content.type = type;
+            seaworld(content);
+        })
+        $('#goSearch').click(function () {
+            var dish = $('#search').val();
+            if(dish == "") {
+                $.alert("请输入您要查询的海鲜")
+            }else {
+                $.router.back();
+                content.type = "";
+                content.dish = dish;
+                seaworld(content);
+            }
+        });
+        $('#searchway').click(function () {
+            $.router.load("#router2");
+        })
+        $('#searchtextBack').click(function () {
+            $.router.back();
+        })
+        $('.searchway-input').on('click',function () {
+            $('#search').focus();
+        })
+    }
+
+    return {
+        init: init
+    }
+})();
+
+$(function () {
+    $.showIndicator();
+    seaworld.init();
+});
