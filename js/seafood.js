fe6282b97fdd8cb2db9eabee64eb079f1f34fb65
Your Name <you@example.com>
2017-06-30 09:13:00 +0800

    d

--- js/seafood.js
+++ js/seafood.js
@@ -1,3 +1,9 @@
 $('#toSeafood').click(function () {
     window.location.href = "feast.html";
 });
+$('#searchway').click(function () {
+    $.router.load("#router2");
+})
+$('#searchtextBack').click(function () {
+    $.router.back();
+})
